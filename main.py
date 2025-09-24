d750e076af5e6cffe8cbd2fa680323c19a1a9a8a
Ajeshwori <adhikariajeshwori099@gmail.com>
2025-09-24 20:02:48 +0545

    Simple calculator

---
+++ main.py
@@ -0,0 +1,32 @@
+from operation import add, subtract, multiply, divide
+from utils import get_number
+
+def main():
+    print("=== Simple Calculator ===")
+    while True:
+        print("\nOptions : +, -, *, /, q (quit)")
+        choice = input("Choose an operation: ").strip()
+
+        if choice == "q":
+            print("Exiting ... Goodbye") 
+            break
+
+        if choice not in ['+', '-', '*', '/']:
+            print("Invalid option")
+            continue
+        
+        num1 = get_number("Enter the first number: ")
+        num2 = get_number("Enter the second number: ")
+
+        if choice == "+":
+            print("Result: ", add(num1, num2))
+        elif choice == "-":
+            print("Result: ", subtract(num1, num2))
+        elif choice == "*":
+            print("Result: ", multiply(num1, num2))
+        elif choice == "/":
+            print("Result: ", divide(num1, num2)) 
+
+if __name__ == "__main__":
+    main()
+
